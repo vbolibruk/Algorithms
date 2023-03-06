1613ea7a736996a3f28d77fa206f87e312942720
Vlad Bolibruk <v.bolibruk@gmail.com>
2023-03-06 08:36:31 +0200

    q2 task2

--- Q2/02/insertionSort.c
+++ Q2/02/insertionSort.c
@@ -1,29 +1,15 @@
 // Випишіть як буде виглядати масив після кожної ітерації алгортму, якщо його початковий стан <31, 41, 59, 26, 41, 58>
-// Answer
-// 1. 26, 41, 59, 31, 41, 58
-// 2. 26, 31 , 59, 41 , 41, 58
-// 3. 26, 31 , 41 , 59 , 41, 58
-// 4. 26, 31 , 41 , 41 ,59, 58
-// 5. 26, 31 , 41 , 41 ,58 , 59
-
 // Як буде працювати алгоритм, якщо його початковий стан <1, 2, 3, 4, 5, 6> ?
-// буде проходити всі ітераціі
-
 // Як буде працювати алгоритм, якщо його початковий стан <6, 5, 4, 3, 2, 1> ?
-// буде проходити всі ітераціі як и вище
-
 // Як буде працювати алгоритм, якщо його початковий стан <3, 3, 3, 3, 3, 3> ?
-// буде проходити всі ітераціі як и вище
-
 // Оцініть час роботи алгоритму в цих 4-х випадках?
-// ON^2
 // Реалізувати алгоритм Selection Sort
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 void swap(int *a, int *b);
-void selectionSort(int array[], int arraySize);
+void insertionSort(int array[], int arraySize);
 
 int main(void)
 {
@@ -42,7 +28,7 @@ int main(void)
         ptr[i] = rand() % 10;
         printf("%d, ", ptr[i]);
     }
-    selectionSort(ptr, arraySize);
+    insertionSort(ptr, arraySize);
     printf("\n");
     printf("The sorted elements of the array are: ");
 
@@ -62,24 +48,20 @@ void swap(int *a, int *b)
 }
 
 // selection sort sample
-void selectionSort(int array[], int arraySize)
+void insertionSort(int array[], int arraySize)
 {
-    int i, j, min_index;
+    int i, j, value;
     int num1, num2;
-    for (i = 0; i < arraySize; i++)
+    for (i = 1; i < arraySize; i++)
     {
-        min_index = i;
-        for (j = i + 1; j < arraySize; j++)
-        {
-            if (array[j] < array[min_index])
-            {
-                min_index = j;
-            }
+        value = array[i];
+        j = i - 1;
 
+        while (j >= 0 && array[j] > value)
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
         }
-                    if (min_index != i)
-            {
-                swap(&array[min_index], &array[i]);
-            }
+        array[j + 1] = value;
     }
 }
